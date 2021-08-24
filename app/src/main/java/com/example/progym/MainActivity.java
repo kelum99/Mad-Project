03466bfa55ab70230476b474de808569557c537a
kelumayuwardhana <kelumayuwardhana@gmail.com>
2021-08-24 12:35:24 +0530

    First Commit

---
+++ app/src/main/java/com/example/progym/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.progym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button lognBtn = findViewById(R.id.loginBtn);
+
+        lognBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
