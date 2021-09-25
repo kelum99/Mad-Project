14e4c684a0b98b2906fa4f784a1cada28281251a
kelumayuwardhana <kelumayuwardhana@gmail.com>
2021-09-25 14:19:19 +0530

    exercise and schedule

--- app/src/main/java/com/example/progym/admin/exercises/ExerciseManagment.java
+++ app/src/main/java/com/example/progym/admin/exercises/ExerciseManagment.java
@@ -31,8 +31,6 @@ public class ExerciseManagment extends AppCompatActivity {
     ArrayList<String> exerciseListItems;
     FirebaseListAdapter adapter;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
